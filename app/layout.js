4347c421ad3010bc939ae3a5de9546934dc10161
mishrashivam38547-cmd <mishrashivam38547@gmail.com>
2026-05-17 16:21:08 +0530

    Add analytics component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
+import { Analytics } from '@vercel/analytics/react';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
