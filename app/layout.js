ae3f925866826eba268e7d3bb25e74b079eb8e6d
mishrashivam38547-cmd <mishrashivam38547@gmail.com>
2026-05-17 16:56:29 +0530

    Integrate Monetag verification and ad script
    Added Monetag verification and advertising script.

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,26 @@
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* यहाँ आपका Monetag का कोड जुड़ गया है */}
+        {/* आपका Monetag वेरिफिकेशन टैग */}
         <meta name="monetag" content="c12f9ee2dc743b905812bb84de581dc8" />
+        
+        {/* आपका Monetag In-Page Push विज्ञापन कोड */}
+        <Script 
+          id="monetag-ad"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(s){
+                s.dataset.zone='11018734';
+                s.src='https://nap5k.com/tag.min.js';
+              })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
+            `
+          }}
+        />
       </head>
       <body>
         {children}
