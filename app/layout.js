b433c2b38cb3310418a76bf99213668200fd2ca1
mishrashivam38547-cmd <mishrashivam38547@gmail.com>
2026-05-18 19:11:49 +0530

    Remove Monetag verification and ad script
    Removed Monetag verification tag and ad script from layout.

--- app/layout.js
+++ app/layout.js
@@ -5,22 +5,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* आपका Monetag वेरिफिकेशन टैग */}
-        <meta name="monetag" content="c12f9ee2dc743b905812bb84de581dc8" />
-        
-        {/* आपका Monetag In-Page Push विज्ञापन कोड */}
-        <Script 
-          id="monetag-ad"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(s){
-                s.dataset.zone='11018734';
-                s.src='https://nap5k.com/tag.min.js';
-              })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')));
-            `
-          }}
-        />
       </head>
       <body>
         {children}
